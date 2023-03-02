5c929525597aac8b25fd5d30571183a06f024b7d
2ga2 <115584448+gahuiLee@users.noreply.github.com>
2023-03-02 18:20:22 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
